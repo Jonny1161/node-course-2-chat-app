e8528ddad725c1d435531952a6424235db13694d
Jonathan Chang <jonathanchang@Jonathans-MBP.fios-router.home>
2018-08-26 16:58:01 -0400

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -17,6 +17,26 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  // socket.emit('newEmail', {
+  //   from: 'mike@example.com',
+  //   text: 'Hey, what is going on?',
+  //   createdAt: 123
+  // });
+
+  socket.emit('newMessage', {
+    from: 'Mike',
+    text: 'Hi this is Mike',
+    createdAt: 123
+  });
+
+  // socket.on('createEmail', (newEmail) => {
+  //   console.log('createEmail', newEmail);
+  // });
+
+  socket.on('createMessage', (newMessage) => {
+    console.log('createMessage', newMessage);
+  });
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
