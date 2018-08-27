8668393169e5a5bee233b4bf43d8b230fdb656ff
Jonathan Chang <jonathanchang@Jonathans-MBP.fios-router.home>
2018-08-26 22:25:24 -0400

    Add form for messages and show incoming messages in browser

--- server/server.js
+++ server/server.js
@@ -38,11 +38,12 @@ io.on('connection', (socket) => {
 
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
 
     //io emits to every single connection; socket emits to only a single connection
     io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from the server');
     // socket.broadcast.emit('newMessage', {
     //   from: message.from,
     //   text: message.text,
