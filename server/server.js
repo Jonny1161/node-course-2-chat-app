013ef68ac2dd9167daf1f5c0bb7cbd646df3d4ed
Jonathan Chang <jonathanchang@Jonathans-MBP.fios-router.home>
2018-08-27 19:54:22 -0400

    Add geolocation support via geolocation API

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
 //setup for Heroku or local environment
@@ -49,6 +49,10 @@ io.on('connection', (socket) => {
     //   text: message.text,
     //   createdAt: new Date().getTime()
     // });
+  });
+
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
   });
 
   socket.on('disconnect', () => {
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generate message', () => {
   it('should generate the correct message object', () => {
@@ -12,5 +12,19 @@ describe('generate message', () => {
     expect(res.from).toBe(from);
     expect(res.text).toBe(text);
     expect(typeof res.createdAt).toBe('number');
+  });
+});
+
+describe('generate location message', () => {
+  it('should generate correct location object', () => {
+      var from = 'Jon';
+      var latitude = 38.869889;
+      var longitude = -77.230834;
+
+      var res = generateLocationMessage(from, latitude, longitude);
+
+      expect(res.from).toBe(from);
+      expect(typeof res.createdAt).toBe('number');
+      expect(res.url).toBe(`https://www.google.com/maps?q=${latitude},${longitude}`)
   });
 });
