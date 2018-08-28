02dd5669f2a6f2b9a4201398bfdbab8936685380
Jonathan Chang <jonathanchang@Jonathans-MBP.fios-router.home>
2018-08-28 15:31:13 -0400

    Format timestamps using momentjs

---
+++ playground/time.js
@@ -0,0 +1,19 @@
+// Jan 1st 1970 00:00:00 am - UNIX epoch
+
+var moment = require('moment');
+
+// var date = new Date();
+//
+// console.log(date.getMonth());
+
+// var date = moment();
+// date.add(100, 'years').subtract(9, 'months');
+// console.log(date.format('MMM Do, YYYY'));
+
+var someTimestamp = moment().valueOf();
+console.log(someTimestamp);
+
+
+var createdAt = 1234;
+var date = moment(createdAt);
+console.log(date.format('h:mm a'))
