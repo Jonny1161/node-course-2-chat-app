7e183243fefab94c775b413241bcea05407304c7
Jonathan Chang <jonathanchang@Jonathans-MBP.fios-router.home>
2018-09-02 09:38:36 -0400

    Send messages to only people in same room

--- server/server.js
+++ server/server.js
@@ -64,10 +64,12 @@ io.on('connection', (socket) => {
 
   //listener for the createMessage event
   socket.on('createMessage', (message, callback) => {
-    console.log('createMessage', message);
+    var user = users.getUser(socket.id);
 
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+    }
     //io emits to every single connection; socket emits to only a single connection
-    io.emit('newMessage', generateMessage(message.from, message.text));
     callback();
     // socket.broadcast.emit('newMessage', {
     //   from: message.from,
@@ -77,7 +79,11 @@ io.on('connection', (socket) => {
   });
 
   socket.on('createLocationMessage', (coords) => {
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    var user = users.getUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+    }
   });
 
   socket.on('disconnect', () => {
